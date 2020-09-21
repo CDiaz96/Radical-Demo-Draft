724e34727ae00c98a9dd5031cac5ed678b8e6a8d
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-09-21 09:21:55 -0400

    Upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Radical-Demo-Draft
+![Log In](public/img/login.png)
+
+
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. run `node server.js`
+2. Navigate to `localhost:8080`
+
+## Credit
+
+Modified from Scotch.io's auth tutorial
---
+++ public/main.js
@@ -0,0 +1,23 @@
+
+var trash = document.getElementsByClassName("fa-trash");
+
+
+
+Array.from(trash).forEach(function (element) {
+  element.addEventListener('click', function () {
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    fetch('messages', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg
+      })
+    }).then(function (response) {
+      window.location.reload()
+    })
+  });
+});
